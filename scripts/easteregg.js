9b8f648c8d39da64766ad30ef77305747d300b2a
ulrikah <ulrikhalmoy@gmail.com>
2018-11-01 11:46:53 +0100

    Add Kamilla's index page

---
+++ scripts/easteregg.js
@@ -0,0 +1,26 @@
+//let title = document.getElementById("navbarTitle");
+document.getElementById("navbarTitle").setAttribute("type","button");
+document.getElementById("navbarTitle").setAttribute("onclick","goWingman()");
+
+
+function goWingman() { //Får en liten mann i wingsuit til å fly over siden når man trykker på Wingsuits
+  var leftmargin = 0;
+  var topmargin = 0;
+  var wingman=document.getElementById("wingman");
+
+  if (wingman.style.display != "block") {
+    wingman.style.display= "block";
+    var id = setInterval(frame , 7);
+    }
+    function frame() {
+      if (leftmargin == 1920) {
+        clearInterval(id);
+        wingman.style.display="none";
+      } else {
+        leftmargin = leftmargin + 2;
+        topmargin = topmargin + 1;
+        wingman.style.left = leftmargin + "px";
+        wingman.style.top = topmargin + "px";
+      }
+  }
+}

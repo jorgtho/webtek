198635b9496bf6e3b69778d7aab3329404f0fe40
ulrikah <ulrikhalmoy@gmail.com>
2018-11-02 12:56:53 +0100

    Add Johannes' music.html

---
+++ music/scripts/menu.js
@@ -0,0 +1,47 @@
+/*
+	
+	CONSIDERATIONS:
+	- update the 'css' attribute instead of 'style' if this is possible without jQuery. better practice?
+		i.e. navbar.css('transition-delay', '0s') instead of navbar.style.transitionDelay = "0s"
+	- transition duration and delays can ofc be changed
+
+*/
+
+let open = false;
+
+let img = document.getElementById("pluss");
+let mnu = document.getElementById("menu"); // naming this 'menu' collides with itemholder.js
+let nav = document.getElementById("navbar");
+let drop = document.getElementById("dropdown"); // naming this 'dropdown' collides with itemholder.js
+
+img.addEventListener("click", displayMenu);
+
+function displayMenu () {
+	rotateMenuIcon();
+	open ? openOverlay() : closeOverlay();
+}
+
+function rotateMenuIcon() {
+	if (open == false) {
+		img.setAttribute("style","transform:rotate(225deg); transition-duration:1s;");
+		open = true;
+	}
+ 	else if (open == true) {
+		img.setAttribute("style","transform:rotate(0deg); transition-duration:1s;");
+   		open = false;
+ 	}
+}
+
+function openOverlay() {
+	nav.style.transitionDelay = "0s"
+    nav.style.backgroundColor="rgba(0,0,0,0)";
+    drop.style.height = "calc(100% - 100px)";
+    mnu.style.overflow = "scroll"; /* make the menu scrollable if screen is to small */
+}
+
+function closeOverlay() {
+    nav.style.transitionDelay = "0.5s"
+	nav.style.backgroundColor="rgba(0,0,0,0)"; // transparent
+    drop.style.height = "0";
+    mnnu.style.overflow = "hidden"; /* hide the menu again */
+}
---
+++ scripts/videoPlayer.js
@@ -0,0 +1,42 @@
+//Youtube Api player from https://developers.google.com/youtube/iframe_api_reference
+
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('musicVideo', {
+    videoId: 'iNyRTw1-BGQ',
+    playerVars: {'rel': 0},
+    height: "100",
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+    console.log("ready");
+    event.target.mute();
+    event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+    console.log("state changed");
+}
+
+function muteButton() {
+  if (player.isMuted()) {
+    player.unMute();
+    console.log("mute")
+    document.getElementById("mute").src = "img/volume.svg";
+  } else {
+    player.mute();
+    console.log("unmute");
+    document.getElementById("mute").src = "img/mute.svg";
+    }
+}
